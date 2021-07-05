f089adf6356bf642ee32d599432395773593af3f
massana2110 <00072416@uca.edu.sv>
2021-07-05 15:36:56 -0600

    finish Tools.java file

--- src/mef3d/Tools.java
+++ src/mef3d/Tools.java
@@ -1,13 +1,15 @@
 import classes.Item;
 import classes.enums.indicators;
 import classes.enums.modes;
+import classes.enums.lines;
+import classes.enums.sizes;
 
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Tools {
 
-    public static void obtenerDatos(FileInputStream file, int nlines, int n, int mode, ArrayList<Item> item_list) {
+    public static void obtenerDatos(File file, int nlines, int n, int mode, ArrayList<Item> item_list) {
         String line;
 
         // TODO: research equivalent '>>' operator
@@ -82,5 +84,93 @@ public class Tools {
         }
     }
 
+    public static void addExtension(String newfilename, String filename, String extension){
+        int ori_length = newfilename.length();
+        int ext_length = extension.length();
+
+        StringBuilder newFileName = new StringBuilder(newfilename);
+        int i;
+
+        for (i = 0; i < ori_length; i++){
+            newFileName.setCharAt(i, filename.charAt(i));
+
+        for (i = 0; i < ext_length; i++)
+            newFileName.setCharAt(ori_length + i, extension.charAt(i));
+
+        newFileName.setCharAt(ori_length + i, '\0') ;
+        }
+    }
+
+    public static void leerMallayCondiciones(Mesh m, String filename) throws IOException {
+        String inputFileName = "";
+
+        float k, Q;
+
+        int nnodes, neltos, ndirich, nneu;
+
+        addExtension(inputFileName, filename, ".dat");
+
+        FileReader file = new FileReader(inputFileName);
+
+        // file >> k >> Q;
+        //cout << "k y Q: "<<k<<" y "<<Q<<"\n";
+        // file >> nnodes >> neltos >> ndirich >> nneu;
+        //cout << "sizes: "<<nnodes<<" y "<<neltos<<" y "<<ndirich<<" y "<<nneu<<"\n";
+
+        m.setParameters(k, Q);
+        m.setSizes(nnodes,neltos,ndirich,nneu);
+        m.createData();
+
+        obtenerDatos(file,lines.SINGLELINE,nnodes,modes.INT_FLOAT_FLOAT_FLOAT,m.getNodes());
+        obtenerDatos(file,lines.DOUBLELINE,neltos,modes.INT_INT_INT_INT_INT,m.getElements());
+        obtenerDatos(file,lines.DOUBLELINE,ndirich,modes.INT_FLOAT,m.getDirichlet());
+        obtenerDatos(file,lines.DOUBLELINE,nneu,modes.INT_FLOAT,m.getNeumann());
+
+        file.close();
+        //Se corrigen los índices en base a las filas que serán eliminadas
+        //luego de aplicar las condiciones de Dirichlet
+        correctConditions(ndirich,m.getDirichlet(),m.getDirichletIndices());
+    }
+
+    public static boolean findIndex(int v, int s, ArrayList<Integer> arr){
+        for (int i = 0; i < s; i++){
+            if (arr.get(i) == v) return true;
+        }
+
+        return false;
+    }
+
+    public static void writeResults(Mesh m, Vector T, String filename) throws IOException {
+        String outputFileName = "";
+
+        ArrayList<Integer> dirich_indices = m.getDirichletIndices();
+        Condition dirich = m.getDirichlet();
+
+        addExtension(outputFileName, filename, ".post.res");
+
+        FileReader file = new FileReader(outputFileName);
+
+        // file << "GiD Post Results File 1.0\n";
+        // file << "Result \"Temperature\" \"Load Case 1\" 1 Scalar OnNodes\nComponentNames \"T\"\nValues\n";
+
+        int Tpos = 0;
+        int Dpos = 0;
+        int n = m.getSize(sizes.NODES);
+        int nd = m.getSize(sizes.DIRICHLET);
+
+        for (int i = 0; i < n; i++){
+            if (findIndex(i+1, nd, dirich_indices)){
+                // file << i+1 << " " << dirich[Dpos].getValue() << "\n";
+                Dpos++;
+            } else {
+                // file << i+1 << " " << T.at(Tpos) << "\n";
+                Tpos++;
+            }
+        }
+
+        // file << "End values\n";
+
+        file.close();
+    }
 }
 
